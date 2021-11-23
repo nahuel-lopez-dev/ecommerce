10ee5dce4a929ba60c6581fae7cad035c71a7f7d
Nahuel-DevOne <nahuel.profesor@gmail.com>
2021-11-23 06:12:18 -0300

    optimizando el código

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,49 +5,87 @@ import { Container, Button } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import CartItem from "../CartItem/CartItem"
 import FormBuyer from "../FormBuyer/FormBuyer"
+import { useState } from "react"
+import 'firebase/firestore'
+import { getFirestore } from "../../Firebase/firebase"
+import firebase from 'firebase'
 import 'firebase/firestore'
 
 const Cart = () => {
-  
+
   const { cart, total, clear } = useContext(Context)
+  const [userInfo, setUserInfo] = useState(null)
+  const [orderId, setOrderId] = useState(null)
 
-  return (
-    <div className="cart">
-        <h2 className="fs-1 p-4 bg-dark text-white text-center">Carrito de compras</h2>
-      <Container className="container align-top justify-content-center p-2 text-center ">
+  const updateUserInfo = (userInfo) => {
+    setUserInfo(userInfo)
+  }
 
-        <Container>
-          {/* Si hay productos en el carrito, los muestra.*/}
-          {cart.length > 0 ? (
-            <>
-              {cart.map((producto) =>
-                <CartItem
-                  key={`cart-${producto.id}`}
-                  id={producto.id}
-                  nombre={producto.nombre}
-                  precio={producto.precio}
-                  imagen={producto.imagen}
-                  cantidad={producto.cantidad}
-                  subtotal={producto.subtotal}
-                />
-              )}
+  const items = cart.map((cart) => ({
+    id: cart.id,
+    title: cart.nombre,
+    quantity: cart.cantidad
+  }))
 
-              <h2 className="display-5">Total del carrito: ${total}</h2>
-              <FormBuyer />  
-            </>
-          )
-            :
-            (
-              <>
-                <p className="display-4">Tu carrito está vacío =(</p>
-                <Button variant="danger bg-gradient" as={Link} to='/'>Volver a inicio</Button>
-              </>
-            )
-          }
-        </Container>
+  const order = {
+    userInfo,
+    item: items,
+    date: firebase.firestore.Timestamp.fromDate(new Date()),
+    total: total,
+    estado: 'Generado',
+  }
+
+  async function createOrder() {
+    const db = getFirestore()
+    const orders = db.collection('orders')
+    try {
+      const { id } = await orders.add(order)
+      alert('El id de su orden es:' + id)
+      setOrderId(id)
+      clear()
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return <>
+    {(cart.length === 0 && orderId === null) && 
+    <div className="col-12 text-center mt-5">
+      <p className="display-4">Tu carrito está vacío =(</p>
+      <Button variant="danger bg-gradient" as={Link} to='/'>ir a comprar</Button>  
+    </div>}
+    {orderId !== null && 
+    <div className="col-12 text-center mt-5 mb-5">
+      <h2 className="display-5 mb-4">Hemos procesado su compra correctamente</h2>
+      <h3 className="fs-4 mb-5 bg-dark bg-gradient text-white p-5">Su nro. de orden es: {orderId}</h3>
+      <Link to="/">
+        <button className="btn btn-danger bg-gradient" >¡¡Gracias por su compra!!</button>
+      </Link>
+    </div>}
+    {cart.length !== 0 && 
+    <>
+      <div className="container mt-3">
+        {cart.map((producto) =>
+          <CartItem
+            key={`cart-${producto.id}`}
+            id={producto.id}
+            nombre={producto.nombre}
+            precio={producto.precio}
+            imagen={producto.imagen}
+            cantidad={producto.cantidad}
+            subtotal={producto.subtotal}
+          />
+        )}
+      </div>
+      <h2 className="display-5 text-center">Total del carrito: ${total}</h2>
+      {!userInfo}
+      <FormBuyer onChange={updateUserInfo} />
+      <Container className="text-center">
+        <Button variant="danger bg-gradient m-2" onClick={() => clear()}>Vaciar carrito</Button>
+        <Button variant="btn btn-dark bg-gradient m-2" disabled={userInfo ? false : true} onClick={createOrder}>Realizar Comprar</Button>
       </Container>
-    </div>
-  )
+    </>}
+  </>
 }
 
 export default Cart
--- src/components/FormBuyer/FormBuyer.js
+++ src/components/FormBuyer/FormBuyer.js
@@ -1,84 +1,46 @@
-import { Context } from "../Context/CartContext"
-import { useContext } from "react"
 import { useState } from "react"
-import { Container, Button } from "react-bootstrap"
-import { getFirestore } from "../../Firebase/firebase"
-import firebase from 'firebase'
-import 'firebase/firestore'
+import { Container } from "react-bootstrap"
 
-const FormBuyer = () => {
+const FormBuyer = ({onChange}) => {
 
-    const { cart, total, clear } = useContext(Context)
-
-    const [formData, setFormData] = useState({
-        nombre: '',
-        tel: '',
-        email: ''
+    const [formUser, setFormUser] = useState({
+        name: '',
+        phone: '',
+        email: '',
+        emailConf: ''
     })
 
-    function handleOnChange(e) {
-
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value
-        })
-    }
-
-    const handleOnSubmit = (e) => {
-        e.preventDefault()
-
-        let orden = {}
-
-        orden.date = firebase.firestore.Timestamp.fromDate(new Date());
-
-        orden.buyer = formData
-
-        orden.total = total;
-
-        orden.items = cart.map(cart => {
-            const id = cart.id;
-            const title = cart.nombre;
-            const price = cart.precio * cart.cantidad;
-
-            return { id, title, price }
-        })
-
-        const db = getFirestore();
-        db.collection('orders').add(orden)
-            .then(resp => alert(`Su código de compra es: ${resp.id}`))
-            .catch(err => console.log(err))
-            .finally(() => setFormData({
-                name: '',
-                tel: '',
-                email: ''
-            }))
+    const handleSubmit = (fieldId, evt) => {
+        const value = evt.target.value
+        const formData = {...formUser, [fieldId]: value}
+        const {name, email, phone} = formData
+        onChange((Object.keys(formData).every(k=> formData[k] !== '') && formData.email === formData.emailConf) ? ({name, email, phone}) : null)
+        setFormUser(formData)
     }
 
     return (
         <div className="container-fluid">
             <div className="row">
-                <form onChange={handleOnChange} onSubmit={handleOnSubmit} action="">
+                <form action="">
                     <Container className="d-flex justify-content-center mt-2">
                         <div className="col-12 col-md-10 col-lg-6 bg-dark p-2 rounded">
-                            <h4 className="text-white">Estos datos son necesarios para poder terminar su compra</h4>
+                            <h4 className="text-white text-center">Estos datos son necesarios para poder terminar su compra</h4>
                             <label htmlFor="nombre"></label>
-                            <input name="nombre" className="form-control" type="text" placeholder="Nombre" defaultValue={formData.nombre} required />
+                            <input name="nombre" className="form-control" type="text" placeholder="Nombre" defaultValue={formUser.name} onChange={(evt) => handleSubmit('name',evt)} id="inputText" required />
 
                             <label htmlFor="tel"></label>
-                            <input type="tel" name="tel" className="form-control" placeholder="Ingrese su teléfono" defaultValue={formData.tel} />
+                            <input type="tel" name="tel" className="form-control" placeholder="Ingrese su teléfono" defaultValue={formUser.phone} onChange={(evt) => handleSubmit('phone', evt)} id="inputTel" />
 
                             <label htmlFor="email"></label>
-                            <input name="email" className="form-control" type="email" placeholder="Ingrese su email" defaultValue={formData.email} required />
+                            <input name="email" className="form-control" type="email" placeholder="Ingrese su email" defaultValue={formUser.email} onChange={(evt) => handleSubmit('email', evt)} id="staticEmail" required />
+                            
+                            <label htmlFor="emailConf"></label>
+                            <input name="emailConf" className="form-control" type="email" placeholder="Ingrese su email" defaultValue={formUser.emailConf} onChange={(evt) => handleSubmit('emailConf', evt)} id="staticEmailConf" required />
                         </div>
-                    </Container>
-                    <Container>
-                        <Button variant="danger bg-gradient m-2" onClick={() => clear()}>Vaciar carrito</Button>
-                        <Button type="submit" variant="dark bg-gradient m-2">Terminar Compra</Button>
                     </Container>
                 </form>
             </div>
         </div>
-
     )
 }
 
